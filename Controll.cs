d811620972e6276ee639a3605859842c4b0ed530
Thrymnar <kozmikshdh@gmail.com>
2025-10-02 06:43:21 +0400

    Thrymnar

--- Controll.cs
+++ Controll.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Controll : MonoBehaviour
 {
+    private int n1;
+    private int n2;
+    public GameObject obj1;
+    public GameObject obj2;
+    void Start()
+    {
+        n1 = 100;
+        n2 = 100;
+    }
     void Update()
     {
         
@@ -13,6 +22,26 @@ public class Controll : MonoBehaviour
         if (other.CompareTag("Barrier"))
         {
             Destroy(gameObject);
+        }
+        if (other.CompareTag("Character1"))
+        {
+            n1 = n1 - 50;
+            Destroy(gameObject);
+            if (n1 == 0)
+            {
+                Destroy(obj1);
+                Debug.Log(n1);
+            }
+        }
+        if (other.CompareTag("Character2"))
+        {
+            n2 = n2 - 50;
+            Destroy(gameObject);
+            if (n2 == 0)
+            {
+                Destroy(obj2);
+                Debug.Log(n2);
+            }
         }
     }
 }
